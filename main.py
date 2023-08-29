0758c2f565df2f3facd8cd91fbd2ae7bedaf3b90
Vuk <vuk@jensenai.com>
2023-08-29 20:13:27 +0200

    pdf uploading and answering works

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ load_dotenv()
 
 # Access the API key using the environment variable name
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEYY")
+openai.api_key = OPENAI_API_KEY
+top_k = 2
 
 # Initialize Pinecone
 def initialize_pinecone():
@@ -26,7 +28,7 @@ def upload_data_to_pinecone(texts, index_name):
     # Initialize Langchain embeddings
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     # Convert and upload data as tuples (ID, vector)
-    data_to_upload = [(str(i), embeddings.embed_query(text)) for i, text in enumerate(texts)]
+    data_to_upload = [(str(i), embeddings.embed_query(text), {"text": text}) for i, text in enumerate(texts)]
     # Upload the data to Pinecone
     index = pinecone.Index(index_name)
     index.delete(delete_all=True)
@@ -35,7 +37,7 @@ def upload_data_to_pinecone(texts, index_name):
 
 
 # Answer questions based on data in Pinecone
-def answer_question(question, index_name, texts):
+def answer_question(question, index_name):
     # Initialize Langchain embeddings
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
@@ -43,20 +45,38 @@ def answer_question(question, index_name, texts):
     question_embedding = embeddings.embed_query(question)
 
     # Search for the most similar embeddings in Pinecone
-    with pinecone.Index(index_name) as index:
-        results = index.query(queries=[question_embedding], top_k=5)
-
+    index = pinecone.Index(index_name)
+    results = index.query(queries=[question_embedding], top_k=top_k, include_metadata=True)
+    # print(str(results['results'][0]['matches']['metadata']['text']))
     # Access the matches correctly
     matches = results['results'][0]['matches']
 
-    relevant_documents = [texts[int(match['id'])] for match in matches]
+    relevant_documents = [match['metadata']['text'] for match in matches]
 
-    return relevant_documents
+    # Concatenate relevant documents into a single text
+    relevant_documents_text = "\n\n".join(relevant_documents)
+
+    # Create a chat prompt with relevant documents and the user's question
+    chat_prompt = f"Relevant Documents:\n{relevant_documents_text}\n\nUser Question: {question}\nAnswer:"
+
+    # Generate an answer using GPT-3.5 Turbo
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": chat_prompt}
+        ]
+    )
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print(response.choices[0].message.content)
+    answer = response.choices[0].message.content
+    return answer
 
 def main():
     # Define your Pinecone index name
     index_name = "test"  # Replace with your desired index name
     texts = []
+    initialize_pinecone()
 
     # Streamlit App
     st.title("PDF Text Extractor and Question Answering")
@@ -77,26 +97,21 @@ def main():
         # Split the text into smaller chunks for embedding and Pinecone upload
         chunk_size = 1000  # You can adjust this based on your needs
         texts = [extracted_text[i:i + chunk_size] for i in range(0, len(extracted_text), chunk_size)]
-
+        for i, text in enumerate(texts):
+            st.subheader("-------------------------------------------------")
+            st.subheader(f"Text Chunk {i}")
+            st.write(text)
         # Upload the extracted text to Pinecone
         upload_data_to_pinecone(texts, index_name)
 
     # Streamlit App (continued)
     st.header("Ask a Question")
-
+    relevant_documents = ''
     # User input for question
     user_question = st.text_input("Enter your question:")
-
     if st.button("Get Answer"):
-
         # Retrieve answers from Pinecone based on the user's question
-        relevant_documents = answer_question(user_question, index_name, texts)
-
-        # Display the relevant documents as answers
-        st.subheader("Answers:")
-        for document in relevant_documents:
-            st.write("----------------------")
-            st.write(document)
-
+        relevant_documents = answer_question(user_question, index_name)
+    st.write(relevant_documents)
 if __name__ == "__main__":
     main()
